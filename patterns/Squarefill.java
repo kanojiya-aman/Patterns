8e6422722a0debb7e5d3387f8bdcfe4a8a6370a9
Aman Kanojiya <amanskanojiya@gmail.com>
2024-03-29 15:39:11 +0530

    commit

--- patterns/Squarefill.java
+++ patterns/Squarefill.java
@@ -2,18 +2,33 @@ package patterns;
 
 import java.util.Scanner;
 
-public class Squarefill{
+public class Squarefill {
     public static void main(String[] args) {
+        // Creating a Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+        
+        // Prompting the user to enter the number of lines (n) for the square
         System.out.println("Enter number of lines(n) :");
+        
+        // Reading the number of lines from the user
         int n = sc.nextInt();
-        int i,j;
-        for(i = 1; i <= n; i++){
-            for(j = 1; j <= n; j++){
-                System.out.print("*"+" ");
+        
+        // Variables to iterate through rows and columns
+        int i, j;
+        
+        // Loop to iterate through each row
+        for(i = 1; i <= n; i++) {
+            // Loop to iterate through each column in the current row
+            for(j = 1; j <= n; j++) {
+                // Printing '*' followed by a space to create the pattern
+                System.out.print("*" + " ");
             }
+            // Moving to the next line to start a new row
             System.out.println();
         }
+        
+        // Closing the Scanner object to prevent resource leak
         sc.close();
     }
 }
+
